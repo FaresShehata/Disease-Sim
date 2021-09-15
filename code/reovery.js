b8d1ae2bd27baae180ba6cd9f60bae92b3b09483
Fares Shehata <68746008+FaresShehata@users.noreply.github.com>
2021-09-15 22:48:41 +0100

    Recovery by chance implemented

---
+++ code/reovery.js
@@ -0,0 +1,18 @@
+function recovery(p) {
+  // This is executed once every second
+
+  for (const person of people) {
+    const possibleStates = [ASYMP, INFECTED, SEVERELYINFECTED];
+    if (!possibleStates.includes(person.state)) continue;
+    // Only considering infected people
+
+    if (!person.inHospital) {
+      if (Math.random() < RECOVERYRATE) person.recover(p);
+    } else {
+      if (Math.random() < HOSPITALRECOVERYRATE) person.recover(p);
+    }
+    /* RECOVERYRATE and HOSPITALRECOVERYRATE are probabilities, so a random
+    number between 0 and 1 is chosen and compared to the parameter. If the
+    random number is less, then the person will recover. */
+  }
+}
